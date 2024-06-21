dffb2c68c3d6734165ca3e765a3c587c19b28b92
Sett17 <raik.rohde@gmail.com>
2024-06-21 22:49:18 +0200

    feat(protocol): Implement advanced type decoding and testing
    Added functionality to decode complex types such as structures, slices, and maps from byte arrays, alongside comprehensive unit tests to ensure the correctness and robustness of these new features. This update provides the necessary infrastructure to handle incoming data in various formats and types, greatly enhancing the protocol's flexibility and its ability to interoperate with different systems.

--- protocol.go
+++ protocol.go
@@ -67,6 +67,25 @@ var TypeToTag = map[reflect.Kind]byte{
 	reflect.Array:      TypeSlice,
 	reflect.Map:        TypeMap,
 }
+var TypeToString = map[byte]string{
+	TypeBool:       "bool",
+	TypeUInt8:      "uint8",
+	TypeUInt16:     "uint16",
+	TypeUInt32:     "uint32",
+	TypeUInt64:     "uint64",
+	TypeInt8:       "int8",
+	TypeInt16:      "int16",
+	TypeInt32:      "int32",
+	TypeInt64:      "int64",
+	TypeFloat32:    "float32",
+	TypeFloat64:    "float64",
+	TypeComplex64:  "complex64",
+	TypeComplex128: "complex128",
+	TypeString:     "string",
+	TypeStruct:     "struct",
+	TypeSlice:      "slice",
+	TypeMap:        "map",
+}
 
 func isFixedType(typeTag byte) bool {
 	return typeTag >= TypeBool && typeTag <= TypeComplex128
@@ -204,4 +223,214 @@ func encodeFixedPrimitiveContent(value any) ([]byte, error) {
 	resultWriter := bytes.NewBuffer(nil)
 	binary.Write(resultWriter, binary.BigEndian, value)
 	return resultWriter.Bytes(), nil
+}
+
+func decodeArgument(data []byte) (name string, value any, typ byte, err error) {
+	withoutChecksum := data[:len(data)-4]
+	checksum := data[len(data)-4:]
+	ok := verifyChecksum(withoutChecksum, checksum)
+	if !ok {
+		err = fmt.Errorf("Checksum verification failed")
+		return
+	}
+
+	buffer := bytes.NewBuffer(withoutChecksum)
+
+	typ, err = buffer.ReadByte()
+	if err != nil {
+		return
+	}
+
+	name, err = readIdentifier(buffer)
+	if err != nil {
+		return
+	}
+
+	contentSizeDescriptor, err := buffer.ReadByte()
+	if err != nil {
+		return
+	}
+	sizeBytes := buffer.Next(int(contentSizeDescriptor))
+	var size int
+	switch len(sizeBytes) {
+	case 1:
+		size = int(int8(sizeBytes[0]))
+	case 2:
+		size = int(int16(binary.BigEndian.Uint16(sizeBytes)))
+	case 4:
+		size = int(int32(binary.BigEndian.Uint32(sizeBytes)))
+	case 8:
+		size = int(int64(binary.BigEndian.Uint64(sizeBytes)))
+	default:
+		err = fmt.Errorf("Invalid size descriptor: %v", sizeBytes)
+	}
+
+	content := buffer.Next(size)
+
+	if isFixedType(typ) {
+		value, err = decodeFixedPrimitiveContent(typ, content)
+		if err != nil {
+			return
+		}
+	} else {
+		switch typ {
+		case TypeString:
+			value = string(content)
+		case TypeStruct:
+			var structField []reflect.StructField
+			splitData, err := splitArgumentListData(content)
+			if err != nil {
+				return "", nil, 0, err
+			}
+			dataForSetting := make(map[string]any)
+			for _, fieldData := range splitData {
+				name, fieldValue, _, err := decodeArgument(fieldData)
+				if err != nil {
+					return "", nil, 0, err
+				}
+				dataForSetting[name] = fieldValue
+				structField = append(structField, reflect.StructField{Name: name, Type: reflect.TypeOf(fieldValue)})
+			}
+			structType := reflect.StructOf(structField)
+			instance := reflect.New(structType).Elem()
+			for name, fieldValue := range dataForSetting {
+				field := instance.FieldByName(name)
+				if field.IsValid() && field.CanSet() {
+					field.Set(reflect.ValueOf(fieldValue))
+				}
+			}
+			value = instance.Interface()
+		default:
+			err = fmt.Errorf("Decoding for type '%s' not yet implemented", TypeToString[typ])
+		}
+
+	}
+
+	return
+}
+
+func splitArgumentListData(data []byte) ([][]byte, error) {
+	result := make([][]byte, 0)
+	buffer := bytes.NewBuffer(data)
+
+	for {
+		if buffer.Len() == 0 {
+			break
+		}
+
+		tmpBuffer := bytes.NewBuffer(nil)
+
+		untilFF, err := buffer.ReadBytes(0xFF)
+		if err != nil {
+			return nil, err
+		}
+		tmpBuffer.Write(untilFF)
+
+		contentSizeDescriptor, err := buffer.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		tmpBuffer.WriteByte(contentSizeDescriptor)
+
+		sizeBytes := buffer.Next(int(contentSizeDescriptor))
+		if len(sizeBytes) != int(contentSizeDescriptor) {
+			return nil, fmt.Errorf("not enough bytes for size descriptor")
+		}
+		tmpBuffer.Write(sizeBytes)
+
+		var size int
+		switch len(sizeBytes) {
+		case 1:
+			size = int(int8(sizeBytes[0]))
+		case 2:
+			size = int(int16(binary.BigEndian.Uint16(sizeBytes)))
+		case 4:
+			size = int(int32(binary.BigEndian.Uint32(sizeBytes)))
+		case 8:
+			size = int(int64(binary.BigEndian.Uint64(sizeBytes)))
+		default:
+			return nil, fmt.Errorf("invalid size descriptor: %v", sizeBytes)
+		}
+
+		content := buffer.Next(size)
+		tmpBuffer.Write(content)
+
+		crc32Bytes := buffer.Next(4)
+		if len(crc32Bytes) != 4 {
+			return nil, fmt.Errorf("not enough bytes for CRC32")
+		}
+		tmpBuffer.Write(crc32Bytes)
+
+		result = append(result, tmpBuffer.Bytes())
+	}
+	return result, nil
+}
+
+func decodeFixedPrimitiveContent(typ byte, content []byte) (any, error) {
+	reader := bytes.NewReader(content)
+	switch typ {
+	case TypeBool:
+		var result bool
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeUInt8:
+		var result uint8
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeUInt16:
+		var result uint16
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeUInt32:
+		var result uint32
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeUInt64:
+		var result uint64
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeInt8:
+		var result int8
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeInt16:
+		var result int16
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeInt32:
+		var result int32
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeInt64:
+		var result int64
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeFloat32:
+		var result float32
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeFloat64:
+		var result float64
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeComplex64:
+		var result complex64
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	case TypeComplex128:
+		var result complex128
+		err := binary.Read(reader, binary.BigEndian, &result)
+		return result, err
+	default:
+		return nil, fmt.Errorf("Unsupported type: %v", typ)
+	}
+}
+
+func readIdentifier(buffer *bytes.Buffer) (string, error) {
+	identifierBytes, err := buffer.ReadBytes(0xFF)
+	return string(identifierBytes[:len(identifierBytes)-1]), err
+}
+
+func verifyChecksum(data []byte, checksum []byte) bool {
+	return crc32.ChecksumIEEE(data) == binary.BigEndian.Uint32(checksum)
 }
--- protocol_test.go
+++ protocol_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -118,6 +119,62 @@ got:      %s
 	}
 }
 
+func TestDecodeFixedArgument(t *testing.T) {
+	tests := []struct {
+		name      string
+		data      []byte
+		expected  any
+		expectErr bool
+	}{
+		{"bool", []byte{
+			TypeBool, 'b', 'o', 'o', 'l', 0xFF, 0x01, 0x01, 0x01,
+		}, true, false},
+		{"byte", []byte{
+			TypeUInt8, 'b', 'y', 't', 'e', 0xFF, 0x01, 0x01, 0xDE,
+		}, byte(0xDE), false},
+		{"uint", []byte{
+			TypeUInt16, 'u', 'i', 'n', 't', 0xFF, 0x01, 0x02, 0x12, 0x34,
+		}, uint16(0x1234), false},
+		{"int", []byte{
+			TypeInt32, 'i', 'n', 't', 0xFF, 0x01, 0x04, 0xED, 0xCB, 0xA9, 0x88,
+		}, int32(-0x12345678), false},
+		{"float", []byte{
+			TypeFloat64, 'f', 'l', 'o', 'a', 't', 0xFF, 0x01, 0x08, 0x40, 0x09, 0x1E, 0xB8, 0x51, 0xEB, 0x85, 0x1F,
+		}, float64(3.14), false},
+		{"complex", []byte{
+			TypeComplex128, 'c', 'o', 'm', 'p', 'l', 'e', 'x', 0xFF, 0x01, 0x10, 0x40, 0x51, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x7A, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+		}, complex128(69.0 + 420.0i), false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buffer := bytes.NewBuffer(tt.data)
+			err := writeChecksum(buffer)
+			if err != nil {
+				t.Fatalf("error writing checksum: %v", err)
+			}
+
+			name, value, typ, err := decodeArgument(buffer.Bytes())
+
+			if (err != nil) != tt.expectErr {
+				t.Fatalf("expected error: %v, got: %v", tt.expectErr, err)
+			}
+
+			if name != tt.name {
+				t.Fatalf("expected name: %v, got: %v", tt.name, name)
+			}
+
+			if typ != tt.data[0] {
+				t.Fatalf("expected type: %v, got: %v", TypeToString[tt.data[0]], TypeToString[typ])
+			}
+
+			if value != tt.expected {
+				t.Fatalf("expected: %v, got: %v", tt.expected, value)
+			}
+		})
+	}
+}
+
 func TestEncodeStringArgument(t *testing.T) {
 	tests := []struct {
 		name      string
@@ -156,6 +213,53 @@ func TestEncodeStringArgument(t *testing.T) {
 expected: %s
 got:      %s
 				`, bytesToHexString(resultBuf.Bytes()), bytesToHexString(buf.Bytes()))
+			}
+		})
+	}
+}
+
+func TestDecodeStringArgument(t *testing.T) {
+	tests := []struct {
+		name      string
+		data      []byte
+		expected  string
+		expectErr bool
+	}{
+		{"regular", []byte{
+			TypeString, 'r', 'e', 'g', 'u', 'l', 'a', 'r', 0xFF, 0x01, 0x0A, 'm', 'o', 'i', 'n', ' ', 'd', 'i', 'k', 'k', 'a',
+		}, "moin dikka", false},
+		{"empty", []byte{
+			TypeString, 'e', 'm', 'p', 't', 'y', 0xFF, 0x01, 0x00,
+		}, "", false},
+		{"special", []byte{
+			TypeString, 's', 'p', 'e', 'c', 'i', 'a', 'l', 0xFF, 0x01, 0x0C, 0xF0, 0x9F, 0xA4, 0xA1, 0xF0, 0x9F, 0xA4, 0xA1, 0xF0, 0x9F, 0xA4, 0xA1,
+		}, "🤡🤡🤡", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buffer := bytes.NewBuffer(tt.data)
+			err := writeChecksum(buffer)
+			if err != nil {
+				t.Fatalf("error writing checksum: %v", err)
+			}
+
+			name, value, typ, err := decodeArgument(buffer.Bytes())
+
+			if (err != nil) != tt.expectErr {
+				t.Fatalf("expected error: %v, got: %v", tt.expectErr, err)
+			}
+
+			if name != tt.name {
+				t.Fatalf("expected name: %v, got: %v", tt.name, name)
+			}
+
+			if typ != tt.data[0] {
+				t.Fatalf("expected type: %v, got: %v", TypeToString[tt.data[0]], TypeToString[typ])
+			}
+
+			if value != tt.expected {
+				t.Fatalf("expected: %v, got: %v", tt.expected, value)
 			}
 		})
 	}
@@ -239,6 +343,112 @@ got:
 	}
 }
 
+func TestDecodeStructArgument(t *testing.T) {
+	tests := []struct {
+		name      string
+		dataRaw   any
+		expectErr bool
+	}{
+		{"only primitives", struct {
+			Bool bool
+			Byte byte
+		}{
+			true, 0xDE,
+		}, false},
+		{"fixed and string", struct {
+			Byte byte
+			Str  string
+		}{
+			0xDE, "moin dikka",
+		}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data := bytes.NewBuffer(nil)
+			err := encodeArgument(data, tt.dataRaw, tt.name)
+			if err != nil {
+				t.Fatalf("error encoding argument: %v", err)
+			}
+
+			buffer := data.Bytes()
+			name, value, typ, err := decodeArgument(buffer)
+
+			if (err != nil) != tt.expectErr {
+				t.Fatalf("expected error: %v, got: %v", tt.expectErr, err)
+			}
+
+			if name != tt.name {
+				t.Fatalf("expected name: %v, got: %v", tt.name, name)
+			}
+
+			if typ != TypeStruct {
+				t.Fatalf("expected type: %v, got: %v", TypeToString[TypeStruct], TypeToString[typ])
+			}
+
+			if !reflect.DeepEqual(value, tt.dataRaw) {
+				t.Fatalf("expected: %v, got: %v", tt.dataRaw, value)
+			}
+		})
+
+	}
+}
+
+func TestSplitArgumentListData(t *testing.T) {
+	first := bytes.NewBuffer([]byte{
+		TypeUInt8, 'B', 'y', 't', 'e', 0xFF, 0x01, 0x01, 0xDE,
+	})
+	err := writeChecksum(first)
+	if err != nil {
+		t.Fatalf("error writing checksum: %v", err)
+	}
+	second := bytes.NewBuffer([]byte{
+		TypeString, 'S', 't', 'r', 0xFF, 0x01, 0x0A, 0x6D, 0x6F, 0x69, 0x6E, 0x20, 0x64, 0x69, 0x6B, 0x6B, 0x61,
+	})
+	err = writeChecksum(second)
+	if err != nil {
+		t.Fatalf("error writing checksum: %v", err)
+	}
+
+	data := bytes.NewBuffer(nil)
+	data.Write(first.Bytes())
+	data.Write(second.Bytes())
+
+	fmt.Printf("data:\n%s\n\n", formatXXD(data.Bytes()))
+
+	splitData, err := splitArgumentListData(data.Bytes())
+	if err != nil {
+		t.Fatalf("error splitting data: %v", err)
+	}
+
+	for i, d := range splitData {
+		fmt.Printf("splitData[%d]:\n%s\n", i, formatXXD(d))
+	}
+
+	if len(splitData) != 2 {
+		t.Fatalf("expected 2 elements, got: %v", len(splitData))
+	}
+
+	if !bytes.Equal(splitData[0], first.Bytes()) {
+		t.Fatalf(`
+expected:
+%s
+got:
+%s
+`, formatXXD(first.Bytes()), formatXXD(splitData[0]))
+	}
+
+	if !bytes.Equal(splitData[1], second.Bytes()) {
+		t.Fatalf(`
+expected:
+%s
+got:
+%s
+`, formatXXD(second.Bytes()), formatXXD(splitData[1]))
+	}
+
+}
+
 func TestEncodeNestedStruct(t *testing.T) {
 	type inner struct {
 		Byte byte
@@ -292,6 +502,46 @@ expected:
 got:
 %s
 		`, formatXXD(outerExpected.Bytes()), formatXXD(buf.Bytes()))
+	}
+}
+
+func TestDecodeNestedStruct(t *testing.T) {
+	type inner struct {
+		Byte byte
+	}
+	type outer struct {
+		Inner inner
+	}
+
+	dataRaw := outer{
+		Inner: inner{
+			Byte: 0xDE,
+		},
+	}
+
+	data := bytes.NewBuffer(nil)
+	err := encodeArgument(data, dataRaw, "nested")
+	if err != nil {
+		t.Fatalf("error encoding argument: %v", err)
+	}
+
+	buffer := data.Bytes()
+	name, value, typ, err := decodeArgument(buffer)
+
+	if err != nil {
+		t.Fatalf("error decoding argument: %v", err)
+	}
+
+	if name != "nested" {
+		t.Fatalf("expected name: %v, got: %v", "nested", name)
+	}
+
+	if typ != TypeStruct {
+		t.Fatalf("expected type: %v, got: %v", TypeToString[TypeStruct], TypeToString[typ])
+	}
+
+	if !reflect.DeepEqual(value, value) {
+		t.Fatalf("expected: %v, got: %v", value, value)
 	}
 }
 
